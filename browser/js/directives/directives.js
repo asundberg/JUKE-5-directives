5b10ccfa345749f0c7e2b08c1eb64c7d83f7ef21
Annika Sundberg <annika.m.h.sundberg@gmail.com>
2016-07-12 20:59:10 -0400

    Completed Directives up until double-click

---
+++ browser/js/directives/directives.js
@@ -0,0 +1,71 @@
+'use strict';
+
+juke.directive('sideBar', function () {
+  return {
+    restrict: 'E',
+    templateUrl: '/js/directives/templates/sidebar.html'
+  };
+});
+
+juke.directive('footerPlayer', function (PlayerFactory) {
+  return {
+    restrict: 'E',
+    templateUrl: '/js/directives/templates/footer.html',
+    link: function (scope, element, attrs) {
+      // angular.extend(scope, PlayerFactory); // copy props from param2 to param1
+
+      scope.toggle = function () {
+        if ( PlayerFactory.isPlaying() ) PlayerFactory.pause();
+        else PlayerFactory.resume();
+      };
+
+      scope.getPercent = function () {
+        return PlayerFactory.getProgress() * 100;
+      };
+
+      scope.handleProgressClick = function (evt) {
+        PlayerFactory.seek(evt.offsetX / evt.currentTarget.scrollWidth);
+      };
+    }
+  };
+});
+
+juke.directive('albumList', function () {
+  return {
+    restrict: 'E',
+    templateUrl: '/js/directives/templates/albumList.html',
+    scope: {
+      albums: '='
+    }
+  };
+});
+
+juke.directive('songList', function (PlayerFactory) {
+  return {
+    restrict: 'E',
+    templateUrl: '/js/directives/templates/songList.html',
+    scope: {
+      songs: '='
+    },
+    link: function (scope, element, attrs) {
+      angular.extend(scope, PlayerFactory); // copy props from param2 to param1
+
+      // scope.artist = theArtist;
+    }
+  };
+});
+
+juke.directive('doubleClick', function (PlayerFactory) {
+  return {
+    restrict: 'A',
+    scope: {
+      doubleClick: '&'
+    },
+    link: function (scope, element) {
+      angular.extend(scope, PlayerFactory);
+      element.on('dblclick', function () {
+        scope.doubleClick();
+      });
+    }
+  };
+});
